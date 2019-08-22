e6e80a8496174b0ad3d067b84415f9823e0bcf19
Matt <matt.dalton@oliveai.com>
2019-08-22 11:06:41 -0400

    Added the signup portion in the users app

--- recap/urls.py
+++ recap/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+def signup(request):
+    if request.method != 'POST':
+        # display blank form.
+        form = CustomUserCreationForm()
+    else:
+        # validate data and send the user to login page.
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            # Q?: What does this actually check for? For example, duplicated
+            # usernames??
+            # If you try to create someone that already exists it will not work
+            # is_valid() must be working in conjunction with the the UserForm
+            # and its specific requirements.
+            
+            # Save the user in DB
+            form.save()
+            # redirect to the login page.
+            # Eventually create this so that the user is automatically logged
+            # in.
+            return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'signup.html', {'form': form})
