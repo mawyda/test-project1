f3391c094b0534a714e8225f6691efcccdc128d2
Matt <matt.dalton@oliveai.com>
2019-08-26 11:11:56 -0400

    Added the password reset flow of 4 urls/views/templates. This is only for a password reset through email. Emails on file ignored in git

--- recap/settings.py
+++ recap/settings.py
@@ -123,6 +123,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+# Emails
+# Current using the dev server
+EMAIL_BACKEND =  "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
